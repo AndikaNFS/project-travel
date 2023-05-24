f91c973a476bfb5e66c0d09890793d85be9aa8c9
Andika Nur Sasmito <56013918+AndikaNFS@users.noreply.github.com>
2023-05-24 19:51:59 +0700

    Update Login & Sign Up

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -18,8 +18,33 @@ const Login = () => {
       // Handle login error
     }
   };
-
   return { email, setEmail, password, setPassword, handleSubmit };
+
+  // return (
+  //   <>
+  //     <div class="login">
+  //       <form onSubmit={handleSubmit}>
+  //         <label for="chk" aria-hidden="true">
+  //           Login
+  //         </label>
+  //         <input
+  //           type="email"
+  //           name="email"
+  //           placeholder="Email"
+  //           value={email}
+  //           onChange={(e) => setEmail(e.target.value)}
+  //         />
+  //         <input
+  //           type="password"
+  //           name="password"
+  //           placeholder="Password"
+  //           onChange={(e) => setPassword(e.target.value)}
+  //         />
+  //         <button type="submit">Login</button>
+  //       </form>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default Login;
