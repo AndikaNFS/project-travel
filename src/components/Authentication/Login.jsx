ef7cfbf471c50032f49723269323041dfd4b633c
ady.to <adiyulianto61@gmail.com>
2023-05-25 17:29:31 +0700

    refresh

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/api/login", { email, password });
+      const response = await axios.post('/api/login', { email, password });
       // Store the authentication token in local storage or cookies
-      localStorage.setItem("token", response.data.token);
+      localStorage.setItem('token', response.data.token);
       // Redirect to the home page or update the authentication state
       // ...
     } catch (error) {
